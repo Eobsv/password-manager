af6d0137b9710921e3f711d0aafc6244e523418a
Eobsv <d.eobsv@gamail.com>
2023-03-17 16:22:49 +0100

    Added part of Backend to GUI.

--- main.py
+++ main.py
@@ -1,16 +1,58 @@
 from tkinter import *
+from tkinter import messagebox
+
 # ---------------------------- PASSWORD GENERATOR ------------------------------- #
 
 # ---------------------------- SAVE PASSWORD ------------------------------- #
+def save():
+    email = email_entry.get()
+    website = website_entry.get()
+    password = password_entry.get()
+
+    if len(website) == 0 or len(password) == 0:
+        messagebox.showwarning(title="Warnign", message="You left some box empty")
+    else:
+        is_ok = messagebox.askokcancel(title="Website", message=f"There are the details entered: \nEmail: {email}"
+                                                            f"\nPassword: {password} \n Is it ok to save?")
+        if is_ok:
+            with open("pws.txt", mode="a") as pws_data:
+                pws_data.write(f"{website} | {email} | {password}\n")
+                website_entry.delete(0, END)
+                password_entry.delete(0, END)
+
 
 # ---------------------------- UI SETUP ------------------------------- #
 window = Tk()
 window.title("PW mng")
-window.config(width=300, height=300, padx=20, pady=20, bg="white")
+window.config(padx=50, pady=50)
 
-canvas = Canvas(width=200, height=189, bg="white", highlightthickness=0)
+canvas = Canvas(width=200, height=200, highlightthickness=0)
 lock_img = PhotoImage(file="logo.png")
 canvas.create_image(100, 100, image=lock_img)
-canvas.pack()
+canvas.grid(row=0, column=1)
+
+# Labels
+website_label = Label(text="Website:")
+website_label.grid(row=1, column=0)
+email_label = Label(text="Email/Username:")
+email_label.grid(row=2, column=0)
+password_label = Label(text="Password:")
+password_label.grid(row=3, column=0)
+
+# Entries
+website_entry = Entry(width=37)
+website_entry.focus()
+website_entry.grid(row=1, column=1, columnspan=2, sticky="EW")
+email_entry = Entry(width=37)
+email_entry.insert(0, "fake@mail.com")
+email_entry.grid(row=2, column=1, columnspan=2, sticky="EW")
+password_entry = Entry(width=21)
+password_entry.grid(row=3, column=1, sticky="EW")
+
+# Buttons
+generate_button = Button(text="Generate Password", highlightthickness=0)
+generate_button.grid(row=3, column=2)
+add_button = Button(text="Add", width=35, command=save)
+add_button.grid(row=4, column=1, columnspan=2, sticky="EW")
 
 window.mainloop()
