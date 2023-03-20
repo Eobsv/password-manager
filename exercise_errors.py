9754bf246db893103508448090695c9edc14293d
Eobsv <d.eobsv@gamail.com>
2023-03-20 23:35:30 +0100

    Added file for trying and getting to know errors and handling them in python.

---
+++ exercise_errors.py
@@ -0,0 +1,23 @@
+# FileNotFound
+# with open("a_file.txt:") as file:
+# file.read()
+try:
+    file = open('a_file.txt')
+    a_dictionary = {"key": "value"}
+    print(a_dictionary["sadasd"])
+except FileNotFoundError:
+    file = open('a_file.txt', mode="w")
+    file.write("Something")
+except KeyError as error_message: # for keeping error msg
+    print(f"The key {error_message} does not exist")
+# KeyError
+# a_dict = {"key": "Value"}
+# value = a_dict{"non_existing_key"}
+
+# IndexError
+# fruit_list = ["apple", "banana", "pear"]
+# fruit = fruit_list['non_existing_index']
+
+# TypeError
+# text = "abc"
+# print(text + 5)
