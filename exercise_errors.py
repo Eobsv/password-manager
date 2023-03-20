3d1d5e167c838032deb76fa40943971b742d995e
Eobsv <d.eobsv@gamail.com>
2023-03-20 23:38:04 +0100

    Further learning for try-except-else-finally structure.

--- exercise_errors.py
+++ exercise_errors.py
@@ -4,12 +4,19 @@
 try:
     file = open('a_file.txt')
     a_dictionary = {"key": "value"}
-    print(a_dictionary["sadasd"])
+    print(a_dictionary["key"])
 except FileNotFoundError:
     file = open('a_file.txt', mode="w")
     file.write("Something")
 except KeyError as error_message: # for keeping error msg
     print(f"The key {error_message} does not exist")
+else:
+    content = file.read()
+    print(content)
+finally:
+    file.close()
+    print("File was closed.")
+
 # KeyError
 # a_dict = {"key": "Value"}
 # value = a_dict{"non_existing_key"}
